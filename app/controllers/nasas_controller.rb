aba1a9b59cddd6ee677edd6ebe15fd1c66c5249f
github username <github email address>
2019-12-24 01:13:16 -0700

    finished Nasa Controller

--- app/controllers/nasas_controller.rb
+++ app/controllers/nasas_controller.rb
@@ -1,12 +1,36 @@
 class NasasController < ApplicationController
 
     def search
-        @files = Nasa.search_by_term(params[:query])
-        render json: @files 
+        @files = Nasa.search_by_term(params[:query]).paginate(page: params[:page], per_page: 15)
+
+        if params[:media]
+            @files.select! do |file|
+                file[:media_type] == params[:media]
+            end
+        end
+
+        if params[:startDate] && params[:endDate]
+            @files.select! do |file|
+                temp = file[:date_created].split('T')[0] 
+                temp >= params[:startDate] && temp <= params[:endDate]
+            end
+        end
+        render json: {
+            file: @files.as_json(only: [:title, :thumb_url]),
+            page: @files.current_page,
+            pages: @files.total_pages
+        }
     end
 
     def show
+        @file = Nasa.find_by(id: params[:id])
 
+        render json: {
+            file: include: {
+                keywords: {only: [:phrase]},
+                hrefs: {only: [:image_size_url]}
+            }
+        }
     end
-
+    
 end
