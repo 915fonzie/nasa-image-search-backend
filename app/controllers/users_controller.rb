113cb5286a4db69fb2bce001b67dfcd65bd9486d
github username <github email address>
2019-12-24 01:24:55 -0700

    added methods to wrong file for user controller, re-adding to correct controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < ApplicationController
+    def show
 
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,3 @@
 class User < ApplicationRecord
 
-    def show
-
-    end
-
-    def create
-
-    end
-
-    def update
-
-    end
-
-    def destroy
-
-    end
-
-    private
-
-    def user_params
-        params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
-    end
 end
