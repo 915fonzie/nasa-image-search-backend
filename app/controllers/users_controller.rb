a90275b606c06fd6a39705c255223c42da13bbb3
github username <github email address>
2019-12-24 15:07:28 -0700

    added to the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,31 @@
 class UsersController < ApplicationController
-    def show
+    before_action :set_user, only: [show, update, destroy]
 
+    def show
+        render json: {
+            user: @user
+        }
     end
 
     def create
-
+        @user = User.create(user_params)
     end
 
     def update
-
+        @user.update(user_params)
     end
 
     def destroy
-
+        @user.destroy
     end
 
     private
 
     def user_params
         params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find_by(id: params[:id])
     end
 end
