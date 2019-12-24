79ad4e2b71e1d97c5169339e3de5828429112577
github username <github email address>
2019-12-24 01:23:19 -0700

    added methods to user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
 class User < ApplicationRecord
 
+    def show
+
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+    end
 end
